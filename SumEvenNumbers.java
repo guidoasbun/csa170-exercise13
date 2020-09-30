b9e9701fab06a01f86eb7930e4d315edcc10736e
guidoasbun <gdrodrigo8@gmail.com>
2020-09-30 16:13:34 -0700

    first

---
+++ SumEvenNumbers.java
@@ -0,0 +1,17 @@
+/* 
+*   Guido Asbun
+*   CS A170 
+*   30-September-2020     
+* 
+*   Exercise 13
+*/ 
+
+import java.util.Scanner;
+public class SumEvenNumbers{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter two integers, I'll check if one is divisible by the other: ");
+     
+    }
+}
